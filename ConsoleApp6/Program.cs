9d8f5aa37516c9555f9d75d987544abf7edd9fe0
kamilcyan <kamil.lucjanek@wp.pl>
2020-04-04 19:43:40 +0200

    Finish. all works

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -7,25 +7,36 @@ namespace ConsoleApp6
         static void Main(string[] args)
         {
             long temporary;
-            char[] converted = new char[12];
+            char[] converted = new char[14];
             long ticks = DateTime.Now.Ticks;
 
 
             Console.Write(ticks.ToString());
             Console.Write("\n");
 
-
-            for (int j = converted.Length - 1; j >= 0; j--)
+            // here we change number of ticks for alphanumerics
+            for (int j = converted.Length - 3; j >= 0; j--)
             {
                 temporary = ticks % 36;
                 converted[j] = change(temporary);
                 ticks = ticks / 36;
             }
 
+            int addition = additions();
+
+            // here we add 3 digit random number, change it for alphanumerics chars and add it at the end of our id number
+            for (int i = converted.Length - 1; i>converted.Length-3; i--)
+            {
+                temporary = addition % 36;
+                converted[i] = change(temporary);
+                addition = addition / 36;
+            }
+
             for (int i = 0; i < converted.Length; i++)
             {
                 Console.Write(converted[i]);
             }
+
             Console.Read();
         }
 
@@ -38,14 +49,12 @@ namespace ConsoleApp6
             return znak;
         }
 
-        public static char additions()
+        public static int additions()
         {
-            char zn;
-
             Random rand = new Random();
             int number = rand.Next(0, 999);
 
-            return zn;
+            return number;
         }
     }
 }
